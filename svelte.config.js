8d2cbe3d933bb19bdd009bc9130bb22326722843
Pablo Carrasco <pablocarrascoegea@gmail.com>
2026-03-21 21:28:19 +0100

    switch to netlify deployment, simplify CI to data sync only

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-const dev = process.argv.includes('dev')
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
       fallback: 'index.html'
-    }),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH || ''
-    }
+    })
   }
 }
 
